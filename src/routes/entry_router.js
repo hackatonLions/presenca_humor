dc3a1ffc586efe97fa6ada1c3746ab1fe0618bd8
João Altevir <j.altevirneto@gmail.com>
2024-11-09 02:51:57 -0300

    feat: protótipo de função de entrada

---
+++ src/routes/entry_router.js
@@ -0,0 +1,10 @@
+import {Router} from "express";
+
+import funcoesEntrada from "../middlewares/entryFunction.js"
+
+const router = Router();
+
+router.post("/:digital", funcoesEntrada);
+
+
+export default router
